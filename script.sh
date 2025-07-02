7778fe6909c8accba8081b14ae5514df953f371c
nadine2000 <107400308+nadine2000@users.noreply.github.com>
2025-07-02 11:09:15 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 sudo apt update
 sudo apt install -y python3-pip git
 
+pip3 install requests
+pip3 install boto3
+
 git clone https://github.com/nadine2000/pokemon
 cd pokemon
-
-pip3 install requests
 
 python3 main.py
